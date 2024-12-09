32119eb221a9f96bbd3d53863b41499e0e05f47d
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-09 08:15:29 +0530

    params set

---
+++ src/app/prediction/[name]/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Home({params}: any) {
+    return (
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            {params.name}
+        </main>
+    )}
