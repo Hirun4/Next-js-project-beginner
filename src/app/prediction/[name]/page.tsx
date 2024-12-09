d8fc888a0eecd80f0c1ff1dd010fcf87806286cb
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-09 08:37:00 +0530

    fetching

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,6 +1,25 @@
 
+const getPredictedAge = async (name:string) => {
+    const res= await fetch(`http://api.agify.io/?name=${name}`);
+    return res.json();
+}
 
-export default function Home({params}: any) {
+const getPredictedAge = async (name:string) => {
+    const res= await fetch(`http://api.agify.io/?name=${name}`);
+    return res.json();
+}
+
+const getPredictedAge = async (name:string) => {
+    const res= await fetch(`http://api.agify.io/?name=${name}`);
+    return res.json();
+}
+
+
+interface Params {
+    params: { name: string};
+}
+
+export default function Home({params}: Params) {
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
             {params.name}
